f9a0d6699e0833afb7b073bc54a50192ca8f43d1
EdwardTL <edward.toledo.lopez@hotmail.com>
2020-06-20 02:01:47 -0500

    Paso la prueba de los valores incrustados

--- BestBooks.py
+++ BestBooks.py
@@ -1,61 +1,65 @@
 import csv, os
+import re
 
 
 def Load_And_Max(file, ColumnName):
-    with open(books, mode="r") as csv_file:
+    with open(books, mode="r", encoding='utf-8') as csv_file:
         csv_reader = csv.DictReader(csv_file)
         max_rate = 0
         ArrayColumn = []
+        
 
         for row in csv_reader:
-            if isinstance(float(row[ColumnName]), float) == True:
+            """Sinceramente, no encontré la forma de hacer que identifique cuando no es un numero."""
+            """Intente RegEx, isalpha y cachar el error en caso de que no pudeira convertir el float."""
+            """Asi que sí me puedes ayudar con esto, te lo agradecería infinitamente por que sí me tocó corregir esto a mano en mis anteriores trabajos"""
+            if len(row[ColumnName]) <= 4: 
                 ArrayColumn.append(float(row[ColumnName]))
+                
 
                 if float(row[ColumnName]) > max_rate:
                     max_rate = float(row[ColumnName])
+            else:
+                ArrayColumn.append("DOSN'T COUNT!")
 
     csv_file.close()
 
     return ArrayColumn, max_rate
 
-def BestRated(file, RateArray, BestRate, TitleHeader, RateHeader):
-    BestRatedArray = RateArray
-    BestRatedDir = []
+def SearchArrayDir(Array, SearchValue):
+    ArrayOfEquals = []
     dir_counter = 0
+    SearchValue = str(SearchValue)
+    ErrorArray = []
 
-    for element in range(len(RateArray)):
-        if RateArray[dir_counter] == BestRate:
-            BestRatedDir.append(dir_counter+1)
-        
+    for element in range(len(Array)):
+        if Array[dir_counter] == "DOSN'T COUNT!":
+            print(f'si entró en {dir_counter} a {Array[dir_counter]}')
+            ErrorArray.append(dir_counter)
+        else:
+            if str(Array[dir_counter]) == SearchValue:
+                    print(f'{dir_counter} Entro {Array[dir_counter]} al == {SearchValue} ')
+                    ArrayOfEquals.append(dir_counter)
         dir_counter += 1
-
-    TitleArray = []
-
-    with open(books, mode="r") as csv_file:
-        csv_reader = csv.DictReader(csv_file)
-        line_counter = 0
-        for row in csv_reader:
-            if line_counter == 0:
-                line_counter += 1
-            TitleArray.append(row[TitleHeader])
+    
+    print(ErrorArray)
+    return ArrayOfEquals
 
 
-    csv_file.close()
-    print(f'Position = {BestRatedDir}')
-    print(' Rate  ', '  Title')
-    print('------ ', ' -------')
-    for i in range(len(BestRatedArray)):
-        print(f' {BestRatedArray[i]}     {TitleArray[i]}')
 
 if __name__=='__main__':
     bookID = []
     title = []
     rate = []
     # THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
-    books = 'files/test01.csv'
+    cwd = os.getcwd()
+    books = os.path.join(cwd,'test01.csv')
 
-    rate, max_rate = Load_And_Max(books,"average_rating")
+    rates, max_rate = Load_And_Max(books,"average_rating")
 
     print(rate)
     print(max_rate)
-    BestRated(books, rate, max_rate, "title", "average_rating")
+    MaxDirs = SearchArrayDir(rates, max_rate)
+    print(MaxDirs)
+    # ExtractTable(books, rate, max_rate, "title", "average_rating")
+
