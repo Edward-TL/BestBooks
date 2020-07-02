60202975fc53fd5d3bb038eb7941155ba1579231
EdwardTL <edward.toledo.lopez@hotmail.com>
2020-07-01 19:30:17 -0500

    +binary_search +top_condioned IMPROVED: top and match cases

--- BooksTools.py
+++ BooksTools.py
@@ -6,8 +6,8 @@ def load_and_max(file, column_to_find):
         csv_reader = csv.reader(csv_file)
         
         header = next(csv_reader)
-        ColumnArray = []
-        Max_value = 0
+        column_array = []
+        max_value = 0
 
         for row in csv_reader:
             try:
@@ -15,40 +15,40 @@ def load_and_max(file, column_to_find):
                 if last_row == '':
                     last_row = row[13]
 
-                if column_to_find == 'average_rating': Column_data = float(row[4])
-                elif column_to_find == 'num_pages': Column_data = int(row[8])
-                elif column_to_find == 'ratings_count': Column_data = int(row[9])
-                elif column_to_find == 'text_reviews_count': Column_data = int(row[10])
+                if column_to_find == 'average_rating': column_data = float(row[4])
+                elif column_to_find == 'num_pages': column_data = int(row[8])
+                elif column_to_find == 'ratings_count': column_data = int(row[9])
+                elif column_to_find == 'text_reviews_count': column_data = int(row[10])
                 else:
-                    ColumnArray = 'Data not Found. Check input'
-                    Max_value = 0
-                    return ColumnArray, Max_value
+                    column_array = 'Data not Found. Check input'
+                    max_value = 0
+                    return column_array, max_value
             except IndexError:
-                if column_to_find == 'average_rating': Column_data = float(row[3])
-                elif column_to_find == 'num_pages': Column_data =  int(row[7])
-                elif column_to_find == 'ratings_count': Column_data = int(row[8])
-                elif column_to_find == 'text_reviews_count': Column_data = int(row[9])
+                if column_to_find == 'average_rating': column_data = float(row[3])
+                elif column_to_find == 'num_pages': column_data =  int(row[7])
+                elif column_to_find == 'ratings_count': column_data = int(row[8])
+                elif column_to_find == 'text_reviews_count': column_data = int(row[9])
                 else:
-                    ColumnArray = 'Data not Found. Check input'
-                    Max_value = 0
-                    return ColumnArray, Max_value
+                    column_array = 'Data not Found. Check input'
+                    max_value = 0
+                    return column_array, max_value
 
-            ColumnArray.append(Column_data)
-            if Column_data > Max_value:
-                Max_value = Column_data
+            column_array.append(column_data)
+            if column_data > max_value:
+                max_value = column_data
 
     csv_file.close()
 
-    return ColumnArray, Max_value
+    return column_array, max_value
 
 def merge_sort(array):
     if len(array) > 1:
         middle = len(array) // 2
-        Left = array[:middle]
-        Right = array[middle:]
+        left = array[:middle]
+        right = array[middle:]
 
-        merge_sort(Left)
-        merge_sort(Right)
+        merge_sort(left)
+        merge_sort(right)
         
         """SubArrays Iterators"""
         i = 0
@@ -56,57 +56,82 @@ def merge_sort(array):
         """MainArray Iterator"""
         k = 0
 
-        while i < len(Left) and j < len(Right):
-            if Left[i] < Right[j]:
-                array[k] = Left[i]
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                array[k] = left[i]
                 i += 1
             else:
-                array[k] = Right[j]
+                array[k] = right[j]
                 j += 1
             
             k += 1
 
-        while i < len(Left):
-            array[k] = Left[i]
+        while i < len(left):
+            array[k] = left[i]
             i += 1
             k += 1
 
-        while j < len(Right):
-            array[k] = Right[j]
+        while j < len(right):
+            array[k] = right[j]
             j += 1
             k += 1
 
     return array
 
-def topx_ordered(array, top_size):
+def binary_search(array, start, end, search_value):
+    if start > end:
+        return end
+    
+    middle = (start + end) // 2
+
+    if array[middle] == search_value:
+        return middle
+    elif array[middle] < search_value:
+        return binary_search(array, middle + 1, end, search_value)
+    else:
+        return binary_search(array, start, middle - 1, search_value)
+    
+def topx(array, top_size):
     """Considerations.
     1) Array is ORDERED from minor to major.
     2) You can insert Arrays with duplicated values.
     3) You won't insert Sets."""
 
-    TopX = []
-    position = len(array) - 1
+    sorted_and_reduced_array = sorted_and_reduce(array.copy())
+    position = len(sorted_and_reduced_array) - 1
     
-    value = array[position]
-    TopX.append(value)
+    topx = []
+    value = sorted_and_reduced_array[position]
+    topx.append(value)
 
     position -= 1
     max_value = value
-    value = array[position]
+    value = sorted_and_reduced_array[position]
 
-    TopValues = 1 
+    top_values = 1 
 
-    while TopValues < top_size and position >= 0 :
+    while top_values < top_size and position >= 0 :
         if value != max_value:
-            TopX.append(array[position])
+            topx.append(sorted_and_reduced_array[position])
             max_value = value
-            TopValues += 1
+            top_values += 1
         position -= 1
-        value = array[position]
+        value = sorted_and_reduced_array[position]
 
         
 
-    return TopX
+    return topx
+
+def top_condisioned(array, start_value):
+    helper_array = sorted_and_reduce(array.copy())
+    helpers_end = len(helper_array) - 1
+    ubication =  binary_search(helper_array, 0, helpers_end, start_value)
+    
+    top_condisioned = []
+    for i in range(ubication, helpers_end):
+        top_condisioned.append(helper_array[i])
+
+    return top_condisioned
 
 def obtain_arrays(file):
     with open(books, mode="r", encoding='utf-8', newline='') as csv_file:
@@ -153,20 +178,20 @@ def obtain_arrays(file):
     return data
 
 def counted_array(elements_to_count, original_array):
-    CountArray = []
+    count_array = []
     for value in range(len(elements_to_count)):
-        CountArray.append(original_array.count(elements_to_count[value]))
+        count_array.append(original_array.count(elements_to_count[value]))
     
-    return CountArray
+    return count_array
 
 def sorted_and_reduce(array):
-    ReducedSet = set(array)
-    ReducedArray = []
-    for element in ReducedSet:
-        ReducedArray.append(element)
-    ReducedArray = merge_sort(ReducedArray)
+    reduced_set = set(array)
+    reduced_array = []
+    for element in reduced_set:
+        reduced_array.append(element)
+    reduced_array = merge_sort(reduced_array)
 
-    return ReducedArray
+    return reduced_array
 
 def match_cases(array1, wanted_array1, array2, wanted_array2):
     """Notice de diference: the original data has to be of the samen lenght,
@@ -188,14 +213,11 @@ def match_cases(array1, wanted_array1, array2, wanted_array2):
             val1 = array1[ub]
             val2 = array2[ub]
             if val1 in wanted_cases_1 and val2 in wanted_array2:
-                print(f'Val 1: {val1}, Val 2: {val2}')
                 match_cases.append(ub)
             
-        
         return match_cases
 
     else: return 'Arrays must have the samen lenght'
-
 
 if __name__=='__main__':
     cwd = os.getcwd()
@@ -203,17 +225,14 @@ if __name__=='__main__':
     
     rating, max_rating = load_and_max(books, 'average_rating')
     reviews, max_reviews = load_and_max(books, 'text_reviews_count')
-    
-    or_rating = rating.copy()
-    or_reviews = reviews.copy()
 
-    ordered_rating = merge_sort(rating)
-    ordered_reviews = merge_sort(reviews)
+    topc_rating = top_condisioned(rating, 4.5)
+    topc_reviews = top_condisioned(reviews, 1000)
 
-    top_rating = topx_ordered(ordered_rating, 32)
-    top_reviews = topx_ordered(ordered_reviews, 952)
+    top_rating = topx(rating, 32)
+    top_reviews = topx(reviews, 952)
     # print(f'TOP5 Rating: {top5_rating} Reviews: {top_reviews}')
     # print(f'RATING: {or_rating[:15]}')
 
-    match_cases = match_cases(or_rating, top_rating, or_reviews, top_reviews)
+    match_cases = match_cases(rating, topc_rating, reviews, topc_reviews)
     print(match_cases)
